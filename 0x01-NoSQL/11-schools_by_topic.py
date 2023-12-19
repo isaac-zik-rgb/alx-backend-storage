fe3fb74519e797c5ddd22aea24d18c58956c7def
Isaac Okechukwu <isaacokechukwu200021@gmail.com>
2023-12-19 22:01:09 +0100

    task11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-"""Python function that returns the list of school having a specific topic:"""
-
+"""
+find by topic
+"""
 import pymongo
 
 
-def def schools_by_topic(mongo_collection, topic):
-    """ Where can I learn Python? """
-    return mongo_collection.find({"topic": topic})
+def schools_by_topic(mongo_collection, topic):
+    """
+    Find by topic
+    """
+    return mongo_collection.find({"topics": topic})
