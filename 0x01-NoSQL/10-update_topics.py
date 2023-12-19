29561d1772e7a3b20e5bd4f75438b2ff6ca77d8e
Isaac Okechukwu <isaacokechukwu200021@gmail.com>
2023-12-19 21:41:24 +0100

    task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+
 """Write a Python function that changes all topics of a school document based on the name:"""
 
 import pymongo
 
 
+
 def update_topics(mongo_collection, name, topics):
     """change school topics"""
+
     return mongo_collection.update_many(
         {"name": name},
-        {$set: {"topics": topics}})
+        {"$set": {"topics": topics}})
