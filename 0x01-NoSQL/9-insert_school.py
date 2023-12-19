5165d97e1f0f473d8764be08a85f468bfd45dc8c
Isaac Okechukwu <isaacokechukwu200021@gmail.com>
2023-12-19 20:31:14 +0100

    task9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,6 +7,6 @@ import pymongo
 def insert_school(mongo_collection, **kwargs):
     """insert a document in python"""
     for key,value in kwargs.items():
-        mongo_collection.insert_many({key: value})
+        mongo_collection.insert({key: value})
 
     return mongo_collection.find({}, {_id:true})
