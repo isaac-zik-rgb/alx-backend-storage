ce8adda093e4783e80bccc72bb7e5cb2ea0e788d
isaac okechukwu chukwuma <isaacokechukwu200021@gmail.com>
2023-12-21 10:56:18 +0100

    task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ data in Redis using the random key and return the key."""
 
 import uuid
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -20,3 +20,19 @@ Redis client as a private variable"""
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Optional[Callable] = None) ->
+    Union[str, int, None]:
+        retrive_data = self._redis.get(key)
+
+        if retrive_data is None:
+            return None
+        return fn(retrive_data) if (fn and callable(fn)) else retrive_data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, fn=lambda data: data.decode("utf-8")
+                        if isinstance(data, byte) else data)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=lambda data: int(data)
+                        if isinstance(data, byte) else data)
